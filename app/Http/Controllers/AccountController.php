525b51f7a36b8cebcbb9fc37180fe95f56975c67
diogogoncalves <dmgdark95@gmail.com>
2025-07-31 21:50:44 +0100

    Update Account

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class AccountController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function show(string $id)
+    {
+        //
+    }
+
+    public function edit(string $id) {}
+}
