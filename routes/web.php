fd3d8bc47af1b32a08a30b38ecf1eb39b8f132ee
diogogoncalves <dmgdark95@gmail.com>
2025-08-12 22:34:28 +0100

    Routes

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,29 @@ Route::group([
     Route::resource('accounts', \App\Http\Controllers\Api\AccountController::class);
 });
 
-Route::resource('accounts', \App\Http\Controllers\AccountController::class);
+Route::resource('accounts', \App\Http\Controllers\AccountController::class, ['except' => ['store', 'update', 'destroy']]);
+
+Route::group([
+    'as' => 'api.',
+    'prefix' => 'api/',
+    'middleware' => 'auth'
+], function () {
+    Route::get('transactions/data', [\App\Http\Controllers\Api\TransactionController::class, 'dataTable']);
+    Route::resource('transactions', \App\Http\Controllers\Api\TransactionController::class, ['except' => ['show', 'create', 'edit']]);
+    Route::post('scheduled-transactions/confirm/{id}', [\App\Http\Controllers\Api\TransactionController::class, 'confirmSheduled']);
+});
+
+Route::resource('transactions', \App\Http\Controllers\AccountController::class, ['except' => ['store', 'update', 'destroy']]);
+
+Route::group(
+    [
+        'as' => 'scheduled-transactions.',
+        'prefix' => 'scheduled-transactions/',
+        'middleware' => 'auth'
+    ],
+    function () {
+        Route::get('/', [\App\Http\Controllers\TransactionController::class, 'index'])->name('index');
+        Route::get('/create', [\App\Http\Controllers\TransactionController::class, 'create'])->name('create');
+        Route::get('/{id}/edit', [\App\Http\Controllers\TransactionController::class, 'edit'])->name('edit');
+    }
+);
