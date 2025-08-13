2a67107bce4bea2cfabaf72b690aa1103e08952c
diogogoncalves <dmgdark95@gmail.com>
2025-08-13 10:59:31 +0100

    Database Migrations

--- database/migrations/2025_07_31_204854_create_transactions_table.php
+++ database/migrations/2025_07_31_204854_create_transactions_table.php
@@ -13,9 +13,19 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('account_id');
             $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('account_id');
+            $table->enum('type', ['expense', 'revenue']);
+            $table->decimal('amount', 15, 2);
+            $table->date('date');
+            $table->string('description')->nullable();
+            $table->enum('status', ['pending', 'completed'])->default('pending');
+            $table->unsignedBigInteger('category_id')->nullable();
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('account_id')->references('id')->on('accounts')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
         });
     }
 
