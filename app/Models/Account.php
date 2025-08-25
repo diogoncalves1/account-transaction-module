a857a95ff86446a1ab9ba158cb4c01fc97bfcf2a
diogogoncalves <dmgdark95@gmail.com>
2025-08-25 13:05:44 +0100

    Models

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Modules\Currency\Entities\Currency;
+use Modules\User\Entities\User;
 
 class Account extends Model
 {
@@ -18,9 +19,22 @@ class Account extends Model
     {
         return $this->hasMany(Transaction::class);
     }
-
     public function currency()
     {
         return $this->belongsTo(Currency::class);
+    }
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'accounts_user', 'account_id', 'user_id')
+            ->using(AccountUser::class)
+            ->withPivot('shared_role_id');
+    }
+
+    public function userSharedRole()
+    {
+        return $this->users()
+            ->where('account_users.user_id', auth()->id())
+            ->join('shared_roles', 'account_users.shared_role_id', '=', 'shared_roles.id')
+            ->first();
     }
 }
--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Modules\Category\Entities\Category;
+use Modules\User\Entities\User;
 
 class Transaction extends Model
 {
@@ -11,4 +13,18 @@ class Transaction extends Model
     use HasFactory;
 
     protected $table = 'transactions';
+    protected $fillable = ['user_id', 'account_id', 'type', 'amount', 'date', 'description', 'status', 'category_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function account()
+    {
+        return $this->belongsTo(Account::class);
+    }
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
