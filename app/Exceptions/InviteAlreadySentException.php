abd26df781b35d477035751a6416522502a5a806
diogogoncalves <dmgdark95@gmail.com>
2025-09-10 19:07:46 +0100

    InviteAlreadySentException

---
+++ app/Exceptions/InviteAlreadySentException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class InviteAlreadySentException extends Exception
+{
+    protected $message;
+    protected $code = 500;
+
+    public function __construct()
+    {
+        parent::__construct(__('exceptions.inviteAlreadySentException'), $this->code);
+    }
+}
