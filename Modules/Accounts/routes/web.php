e504a074dec1f5f78d4d11daabccb0bcb07b0ef0
diogogoncalves <dmgdark95@gmail.com>
2025-11-18 14:41:37 +0000

    Update Routes

--- Modules/Accounts/routes/web.php
+++ Modules/Accounts/routes/web.php
@@ -1,8 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Accounts\Http\Controllers\AccountsController;
-
-// Route::middleware(['auth', 'verified'])->group(function () {
-//     Route::resource('accounts', AccountsController::class)->names('accounts');
-// });
